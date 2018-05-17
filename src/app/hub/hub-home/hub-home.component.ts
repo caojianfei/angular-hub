df3d1f69a76259d9d80e58936d0765e639d2c9c6
caojianfei <120631902@qq.com>
2018-05-17 16:17:16 +0800

    hub header

---
+++ src/app/hub/hub-home/hub-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hub-home',
+  templateUrl: './hub-home.component.html',
+  styleUrls: ['./hub-home.component.scss']
+})
+export class HubHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
